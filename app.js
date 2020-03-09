472fac07aa871d2210cefed028923cdb98ecd7f2
carsonbutterfield <carsonbutterfield98@gmail.com>
2020-03-09 09:42:29 -0700

    comments

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+//this is a project I worked on for an afternoon to better understand websockets so I could assist some students in my class
+
 const WebSocket = require('ws')
 const port = process.env.PORT || 4000
-
+//rooms are WIP
 class Room {
   constructor(creator) {
     this.members = [creator]
@@ -21,7 +23,7 @@ if (rooms[parsed.newroom]) {
         ws.send(JSON.stringify(rooms[parsed.newroom].messages))
       }
 }
-
+//slightly unfinished as Im building rooms out, originally all the users connected were stored together
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     const parsed = JSON.parse(message)
@@ -33,6 +35,7 @@ wss.on('connection', function connection(ws) {
     }
     if(type === "message"){
       rooms[room].messages.push(content)
+      //without rooms this was basically all it needed to work, just replaces rooms[room] with an array of all users
       rooms[room].members.forEach(user => {
         user.send(content)
       })
